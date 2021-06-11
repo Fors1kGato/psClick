405c6aefa405e67e1df4c2c5dbe01167285f5545
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-06-11 20:55:57 +0300

    Find-Image v1

---
+++ psClick_Imaging/Find-Image.ps1
@@ -0,0 +1,26 @@
+﻿function Find-Image{
+    #.COMPONENT
+    #1
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+        $smallPath,
+        $bigPath,
+        $deviation = 0,
+        $accuracy  = 100
+    )
+
+    if(-not ('Drawing.Bitmap'-as [Type]))
+    {[Void][Reflection.Assembly]::LoadWithPartialName("System.Drawing")}
+    if(-not ('ImgSearcher'-as [Type]))
+    {[Void][Reflection.Assembly]::Loadfile("$PSScriptRoot\psClick_SearchImage.dll")}
+
+    $small = [Drawing.Bitmap]::new($smallPath)
+    $big   = [Drawing.Bitmap]::new($bigPath)
+
+    $res = [ImgSearcher]::searchBitmap($small, $big, $deviation, $accuracy)
+
+    $small.Dispose()
+    $big.Dispose()
+    return $res
+}

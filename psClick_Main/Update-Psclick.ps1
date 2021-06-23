3709433665484c5e45eb38dda79b884b3f0e4d72
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-06-23 21:36:41 +0300

    Add files via upload

--- psClick_Main/Update-Psclick.ps1
+++ psClick_Main/Update-Psclick.ps1
@@ -8,13 +8,30 @@
     $url   = "api.github.com/repos/Fors1kGato/psClick/git/trees/main?recursive=1"
     $tree  = (Irm $url -useb).tree
     $files = $tree|?{$_.type -ne "tree"}
-    $tr = gc "$env:USERPROFILE\Documents\psClick\psClick.sha"|ConvertFrom-Json
+    $tr    = (gci -file -Recurse "$env:USERPROFILE\Documents\psClick").FullName|ForEach{
+        $bytes =  [IO.File]::ReadAllBytes($_)
+        $blob  = [Text.Encoding]::UTF8.GetBytes("blob $($bytes.Count)`0") + $bytes
+        $sha1  = [Security.Cryptography.SHA1Managed]::Create()
+        $hash  = $sha1.ComputeHash($blob)
+        $hashString = [BitConverter]::ToString($hash).Replace('-','').ToLower()
+        [PSCustomObject]@{
+            Path = $_.replace("$env:USERPROFILE\Documents\psClick\","").replace("\","/")
+            sha = $hashString
+        }
+        #sleep 50
+    }
 
     $files|%{
         $check = $tr.sha.Contains($_.sha)
         if(!$check){
-            Irm -useb "github.com/Fors1kGato/psClick/raw/main/$($_.path)" -OutFile `
-            (ni (join-path "$env:USERPROFILE\Documents\psClick" $_.path) -Force)
+            try{$p = (Ni (Join-path "$env:USERPROFILE\Documents\psClick" $_.path)  -Force)}
+            catch{
+                Write-Warning (
+                    "Не удалось обновить "+$_.path+
+                    "`nЗакройте программу, которая использует файл в данный момент, и повторите попытку"
+                )
+            }
+            Irm -useb "github.com/Fors1kGato/psClick/raw/main/$($_.path)" -OutFile $p|out-null
         }
     }
 
@@ -22,6 +39,5 @@
     ?{$tree.path -notcontains $_}|%{
         ri (Join-Path "$env:USERPROFILE\Documents\psClick" $_) -Recurse -ea 0
     }
-    $tree|select path, sha|ConvertTo-Json|out-file "$env:USERPROFILE\Documents\psClick\psClick.sha"
     (Get-Command -Module psClick*).Module|ForEach{Remove-Module $_}
 }

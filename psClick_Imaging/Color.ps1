77194cf77667584b764cd1ea33336d81be36f7b6
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-06-28 23:55:14 +0300

    Add files via upload

--- psClick_Imaging/Color.ps1
+++ psClick_Imaging/Color.ps1
@@ -1,12 +1,17 @@
 ﻿function Get-Color{
     #.COMPONENT
-    #1
+    #1.1
     #.SYNOPSIS
     #Author: Fors1k ; Link: https://psClick.ru
     Param(
-        [int]$x,
-        [int]$y,
-        [IntPtr]$handle = [IntPtr]::Zero
+        [int]$X,
+        [int]$Y,
+        [IntPtr]$Handle = [IntPtr]::Zero
     )
+    if($handle){
+        $pt = [Drawing.Point]::new($x, $y)
+        [Void][w32Windos]::MapWindowPoints($Handle, [IntPtr]::Zero, [ref]$pt, 1)
+        $x = $pt.X ; $y = $pt.Y
+    }
     [psClickColor]::GetColor($x, $y, $handle)
 }

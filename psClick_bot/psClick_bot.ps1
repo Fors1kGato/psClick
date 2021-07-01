c7b38e3226769631a935abacb72c78712f060f6c
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-07-02 01:27:31 +0300

    TG bot v1. msg

---
+++ psClick_bot/psClick_bot.ps1
@@ -0,0 +1,18 @@
+﻿function Send-TelegramMessage
+{
+    #.COMPONENT
+    #1
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+        [Parameter(Mandatory)]
+        [String[]]$Message
+    )
+    $WC = [Net.WebClient]::new()
+    $Message|Out-File "$env:TEMP\post_msg.txt" -Encoding utf8
+    $url = "ftp://vh218.timeweb.ru/$me/post_msg.txt"
+    $Wc.Credentials = [Net.NetworkCredential]::new("fors1k_client", "4V8biMPJ");
+    $WC.UploadFile($url,"$env:TEMP\post_msg.txt")
+    Iwr "https://psclick.ru/bots/index.php?a=$mee" -useb|Out-Null
+    $WC.Dispose()
+}

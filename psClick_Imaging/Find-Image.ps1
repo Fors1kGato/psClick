04ee47be76f9a9fcb804a69a598930c81f4a0a13
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-07-23 18:41:23 +0300

    Add files via upload

--- psClick_Imaging/Find-Image.ps1
+++ psClick_Imaging/Find-Image.ps1
@@ -1,4 +1,4 @@
-﻿function Find-Image
+﻿function Find-Image1
 {
     #.COMPONENT
     #3
@@ -89,6 +89,7 @@
         {
             if($StartPos -isnot [Drawing.Point]){try{$StartPos = [Drawing.Point]::new.Invoke($StartPos)}catch{throw $_}}
             if($EndPos -isnot [Drawing.Point]){try{$EndPos = [Drawing.Point]::new.Invoke($EndPos)}catch{throw $_}}
+            $rect = [Drawing.Rectangle]::new($StartPos, ($EndPos.X-$StartPos.X), ($EndPos.Y-$StartPos.Y))
         }
         'Window*'
         {

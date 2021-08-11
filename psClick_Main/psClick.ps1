815af7bbccfc6c59f1bdd254e097ddf233e46e02
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-08-11 21:28:46 +0300

    Delete psClick.ps1

--- psClick_Main/psClick.ps1
+++
@@ -1,9 +0,0 @@
-﻿function psClick{
-    #.COMPONENT
-    #1
-    #.SYNOPSIS
-    #Author: Fors1k ; Link: https://psClick.ru
-    Param(
-    )
-    start "$psscriptroot\psCLick.exe" -WorkingDirectory (split-path "$psscriptroot\psCLick.exe")
-}

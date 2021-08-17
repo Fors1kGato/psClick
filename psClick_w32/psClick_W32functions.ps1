ae77ab72edaec9a0a9435735f250d4b244c5e824
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-08-17 19:00:53 +0300

    Add files via upload

--- psClick_w32/psClick_W32functions.ps1
+++ psClick_w32/psClick_W32functions.ps1
@@ -35,7 +35,7 @@
 function Read-ProcessMemory
 {
     #.COMPONENT
-    #1
+    #2
     #.SYNOPSIS
     #Author: Fors1k ; Link: https://psClick.ru
     [CmdletBinding(DefaultParameterSetName = 'Type')]
@@ -47,24 +47,64 @@ function Read-ProcessMemory
         [Diagnostics.Process]$Process
         ,
         [Parameter(Mandatory, Position = 2, ParameterSetName = "Type")]
-        [ValidateSet('Int16','Int32','Int64','UInt16','UInt32','UInt64','Single','Double')]
+        [ValidateSet('ANSI','Unicode','Int16','Int32','Int64','UInt16','UInt32','UInt64','Single','Double')]
         [String]$Read
         ,
         [Parameter(Mandatory, Position = 2, ParameterSetName = "Size")]
         [Int]$Size
     )
-    if($size){$Data = [byte[]]::new($size)}
-    else{$Data = [byte[]]::new([Runtime.InteropServices.Marshal]::SizeOf([type]$Read))}
     [UInt32]$BytesRead = 0
+    if($Read -notin ('ANSI', 'Unicode')){
+        if($size){$Data = [byte[]]::new($size)}
+        else{$Data = [byte[]]::new([Runtime.InteropServices.Marshal]::SizeOf([type]$Read))}
 
-    $CallResult = [w32]::ReadProcessMemory(
-        $Process.Handle,
-        $Address,
-        $Data,
-        $Data.Count,
-        [ref]$BytesRead
-    )
-    $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
-    if(!$CallResult){throw "Read process memory error: $err"}
-    if(!$size){[BitConverter]::"To$Read"($Data,0)}else{$Data}
+        $CallResult = [w32Memory]::ReadProcessMemory(
+            $Process.Handle,
+            $Address,
+            $Data,
+            $Data.Count,
+            [ref]$BytesRead
+        )
+        $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        if(!$CallResult){throw "Read process memory error: $err"}
+        if(!$size){[BitConverter]::"To$Read"($Data,0)}else{$Data}
+    }
+    else{
+        $memInfo = [w32Memory+MEMORY_BASIC_INFORMATION]::new()
+
+        $queryResult = [w32Memory]::VirtualQueryEx(
+            $Process.Handle,
+            $Address,
+            [ref]$memInfo,
+            [Runtime.InteropServices.Marshal]::SizeOf($memInfo)
+        )
+        $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        if(!$queryResult){throw "Read process memory error: $err"}
+
+        [int]$size = $memInfo.RegionSize - ([int64]$Address - [int64]$memInfo.BaseAddress)
+        $string = [System.Text.StringBuilder]::new($size)
+        if($read -eq 'Unicode'){
+            $CallResult = [w32Memory]::ReadProcessMemoryUnicode(
+                $Process.Handle,
+                $Address,
+                $string,
+                $size,
+                [ref]$bytesRead
+            )
+            $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+            if(!$CallResult){throw "Read process memory error: $err"}
+        }
+        else{
+            $CallResult = [w32Memory]::ReadProcessMemoryAnsi(
+                $Process.Handle,
+                $Address,
+                $string,
+                $size,
+                [ref]$bytesRead
+            )
+            $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+            if(!$CallResult){throw "Read process memory error: $err"}
+        }
+        $string.ToString()
+    }
 }

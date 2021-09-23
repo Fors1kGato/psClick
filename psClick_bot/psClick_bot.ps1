aa3a9a5bffb3a851dd10a9938a658f2fb3f64fea
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-09-23 15:12:43 +0300

    Sending any files

---
+++ psClick_bot/psClick_bot.ps1
@@ -0,0 +1,39 @@
+﻿function Send-TelegramMessage
+{
+    #.COMPONENT
+    #2
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+        [Parameter(Mandatory)]
+        [String[]]$Message
+    )
+    ."$psscriptroot\psClick_bot.exe" -id="$me" -content="$message" -msg
+}
+
+function Send-TelegramFile
+{
+    #.COMPONENT
+    #1
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+        [Parameter(Mandatory)]
+        [String]$Path
+    )
+    ."$psscriptroot\psClick_bot.exe" -id="$me" -content="$path"
+}
+
+function Send-TelegramImage
+{
+    #.COMPONENT
+    #1
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+        [Parameter(Mandatory)]
+        [Drawing.Bitmap]$Image
+    )
+    $Image.Save("$env:TEMP\picture.png", [System.Drawing.Imaging.ImageFormat]::Png)
+    ."$psscriptroot\psClick_bot.exe" -id="$me" -content="$env:TEMP\picture.png"
+}

8ca96900b989d14d439970440b59ec68ea8efd59
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-10-20 00:49:02 +0300

    Add files via upload

--- psClick_w32/psClick_Keyboard.ps1
+++ psClick_w32/psClick_Keyboard.ps1
@@ -1,9 +1,10 @@
 ﻿function Get-KeyState
 {
     #.COMPONENT
-    #2
+    #2.1
     #.SYNOPSIS
     #Author: Fors1k ; Link: https://psClick.ru
+    [Alias('Clear-KeyState')]
     Param(
         [parameter(Mandatory=$true)]
         [ValidateSet('None','LButton','RButton','Cancel','MButton','XButton1','XButton2','Back','Tab','LineFeed','Clear','Enter','Return','ShiftKey','ControlKey','Menu','Pause','CapsLock','Capital','HangulMode','HanguelMode','KanaMode','JunjaMode','FinalMode','KanjiMode','HanjaMode','Escape','IMEConvert','IMENonconvert','IMEAccept','IMEAceept','IMEModeChange','Space','Prior','PageUp','PageDown','Next','End','Home','Left','Up','Right','Down','Select','Print','Execute','Snapshot','PrintScreen','Insert','Delete','Help','D0','D1','D2','D3','D4','D5','D6','D7','D8','D9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','LWin','RWin','Apps','Sleep','NumPad0','NumPad1','NumPad2','NumPad3','NumPad4','NumPad5','NumPad6','NumPad7','NumPad8','NumPad9','Multiply','Add','Separator','Subtract','Decimal','Divide','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','F13','F14','F15','F16','F17','F18','F19','F20','F21','F22','F23','F24','NumLock','Scroll','LShiftKey','RShiftKey','LControlKey','RControlKey','LMenu','RMenu','BrowserBack','BrowserForward','BrowserRefresh','BrowserStop','BrowserSearch','BrowserFavorites','BrowserHome','VolumeMute','VolumeDown','VolumeUp','MediaNextTrack','MediaPreviousTrack','MediaStop','MediaPlayPause','LaunchMail','SelectMedia','LaunchApplication1','LaunchApplication2','OemSemicolon','Oem1','Oemplus','Oemcomma','OemMinus','OemPeriod','Oem2','OemQuestion','Oem3','Oemtilde','Oem4','OemOpenBrackets','OemPipe','Oem5','OemCloseBrackets','Oem6','OemQuotes','Oem7','Oem8','Oem102','OemBackslash','ProcessKey','Packet','Attn','Crsel','Exsel','EraseEof','Play','Zoom','NoName','Pa1','OemClear','KeyCode','Shift','Control','Alt','Modifiers')]
@@ -11,9 +12,12 @@
         ,
         [Switch]$Toggle
     )
+    if($MyInvocation.InvocationName -match 'Clear'){
+        [Void][w32KeyBoard]::GetKeyState([Windows.Forms.Keys]::$key);return
+    }
     if($Toggle){
         if($Key-notin("Scroll","NumLock","CapsLock")){
-            $err = "Укажите одну из следующих клавиш: Scroll ; NumLock ; CapsLock"
+            $err = "Укажите одну из следующих клавиш: [Scroll]  [NumLock]  [CapsLock]"
             throw $err
         }
         [w32KeyBoard]::GetKeyState([Windows.Forms.Keys]::$key)

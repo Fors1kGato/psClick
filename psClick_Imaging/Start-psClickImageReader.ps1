3d0171ecf202f1ce446d212b4f305e04feb5344f
Fors1k <68744677+Fors1kGato@users.noreply.github.com>
2022-05-10 18:29:34 +0300

    Add files via upload

---
+++ psClick_Imaging/Start-psClickImageReader.ps1
@@ -0,0 +1,9 @@
+﻿function Start-psClickImageReader{ 
+    #.COMPONENT
+    #1
+    #.SYNOPSIS
+    #Author: Fors1k ; Link: https://psClick.ru
+    Param(
+    )
+    start "$psscriptroot\psClick — ImageReader.exe" -WorkingDirectory $psscriptroot
+}

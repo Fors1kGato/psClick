d20dbdbb33edb9db9532e2cd4b22391ae8137603
Fors1k <68744677+Fors1kGato@users.noreply.github.com>
2022-05-10 19:17:43 +0300

    Delete Start-psClickImageReader.ps1

--- psClick_Imaging/Start-psClickImageReader.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Start-psClickImageReader{ 
-    #.COMPONENT
-    #1
-    #.SYNOPSIS
-    #Author: Fors1k ; Link: https://psClick.ru
-    Param(
-    )
-    start "$psscriptroot\psClick — ImageReader.exe" -WorkingDirectory $psscriptroot
-}

9c58523cf69aaacf504af9e89da6f03131539a3a
Fors1k <68744677+Fors1kGato@users.noreply.github.com>
2022-05-12 23:12:48 +0300

    Add files via upload

--- psClick_bot/psClick_bot.ps1
+++ psClick_bot/psClick_bot.ps1
@@ -1,43 +1,88 @@
-﻿function Send-TelegramMessage
+﻿function Send-TelegramDocument
 {
     #.COMPONENT
-    #2.1
+    #1
     #.SYNOPSIS
     #Author: Fors1k ; Link: https://psClick.ru
+    [Alias(
+        'Send-TelegramAudio', 
+        'Send-TelegramVideo',
+        'Send-TelegramPhoto'
+    )]
     Param(
-        [Parameter(Mandatory)]
-        [String[]]$Message
+        [Parameter(Mandatory,Position=0)]
+        [Alias("Photo", "Video","Audio")]
+        $File,
+        [Parameter(Position=1)]
+        [String]$Caption,
+        [Parameter(Position=2)]
+        [ValidateSet('MarkdownV2', 'HTML', 'Markdown')]
+        [String]$Parse_mode,
+        [Switch]$Disable_notification,
+        [Int]$Reply_to_message_id,
+        [String]$FileName
     )
-    ."$psscriptroot\psClick_bot.exe" -id="$mee" -content="$message" -msg|out-null
+    if($FileName -and !(Test-Path $FileName -IsValid))
+    {throw "Имя файла содержит недопустимые знаки"}
+    if($File -isnot [Drawing.Bitmap] -and !(Test-Path $File))
+    {throw "Укажите путь существующего файла"}
+    $body = [Collections.Generic.Dictionary[String,String]]::new()
+    $body.Add("chat_id", $me)
+    if($MyInvocation.InvocationName.EndsWith("Document"))
+    {$body.Add("disable_content_type_detection",$true)}
+    ForEach($k in $MyInvocation.BoundParameters.Keys){
+        if($k -eq 'Path'){continue}
+        $body.Add($k.ToLower(), $MyInvocation.BoundParameters.$k)
+    }
+    if($MyInvocation.InvocationName.EndsWith("Photo") -and $File -is [Drawing.Bitmap]){
+        $stream = [IO.MemoryStream]::new()
+        $File.Save($stream, [Drawing.Imaging.ImageFormat]::Png)
+        $stream.Position = 0
+        if(!$FileName){$FileName = Get-Date -Format "dd.MM.yyyy_HH.mm.ss"}
+    }
+    else{
+        $stream = [IO.File]::OpenRead($File)
+        if(!$FileName){$FileName = Split-Path $File -Leaf}
+    }
+    try{
+    $result = [psClick.Telegram]::SendFile(
+        $body,
+        $stream,
+        $FileName,
+        [psClick.Telegram+FileType]::($MyInvocation.InvocationName.Replace("Send-Telegram",""))
+    )
+    if($result.Result){$result = $result.Result|ConvertFrom-Json}
+    }
+    catch{
+        $stream.Close()
+        throw $_
+    }
+    $stream.Close()
+    return $result
 }
 
-function Send-TelegramFile
+function Send-TelegramMessage
 {
     #.COMPONENT
-    #1.1
+    #1
     #.SYNOPSIS
     #Author: Fors1k ; Link: https://psClick.ru
     Param(
-        [Parameter(Mandatory)]
-        [String]$Path
+        [Parameter(Mandatory,Position=0)]
+        [String]$Text,
+        [Parameter(Position=1)]
+        [ValidateSet('MarkdownV2', 'HTML', 'Markdown')]
+        [String]$Parse_mode,
+        [Switch]$Disable_web_page_preview,
+        [Switch]$Disable_notification,
+        [Int]$Reply_to_message_id
     )
-    ."$psscriptroot\psClick_bot.exe" -id="$mee" -content="$path"|out-null
-}
-
-function Send-TelegramImage
-{
-    #.COMPONENT
-    #2
-    #.SYNOPSIS
-    #Author: Fors1k ; Link: https://psClick.ru
-    Param(
-        [Parameter(Mandatory = $true)]
-        [Drawing.Bitmap]$Image
-        ,
-        [Parameter(Mandatory = $false)]
-        [String]$Name
-    )
-    if(!$Name){$Name = "picture"}
-    $Image.Save("$env:TEMP\$Name.png", [Drawing.Imaging.ImageFormat]::Png)
-    ."$psscriptroot\psClick_bot.exe" -id="$mee" -content="$env:TEMP\$Name.png"|out-null
+    $body = [Collections.Generic.Dictionary[String,String]]::new()
+    $body.Add("chat_id",$me)
+    ForEach($k in $MyInvocation.BoundParameters.Keys){
+        $body.Add($k.ToLower(), $MyInvocation.BoundParameters.$k)
+    }
+    $result = [psClick.Telegram]::SendMessage($body)
+    if($result.Result){$result = $result.Result|ConvertFrom-Json}
+    return $result
 }

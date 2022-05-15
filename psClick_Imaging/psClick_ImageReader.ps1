c02e0f33aeb5af31c053b801ce7db67eada562fd
Fors1k <68744677+Fors1kGato@users.noreply.github.com>
2022-05-16 00:56:31 +0300

    Add files via upload

--- psClick_Imaging/psClick_ImageReader.ps1
+++ psClick_Imaging/psClick_ImageReader.ps1
@@ -9,51 +9,67 @@
     start "$psscriptroot\psClick — ImageReader.exe" -WorkingDirectory $psscriptroot
 }
 
-function Recognize-Text 
+function Get-SymbolsBase 
 {
     #.COMPONENT
     #1
     #.SYNOPSIS
     #Author: Fors1k, Cirus ; Link: https://psClick.ru
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory,Position=0)][Alias("Image")]
-        $Path
-    )
+    [CmdletBinding()]Param()
     DynamicParam {
         $attribute = [Management.Automation.ParameterAttribute]@{Mandatory = $true;Position = 1}
         $collection = [Collections.ObjectModel.Collection[System.Attribute]]::new()
         $collection.Add($attribute)
 
-        $validationSet = [String[]](Get-ChildItem "$PSScriptRoot\ImageReader_Data"|Select BaseName -Unique).BaseName
+        $validationSet = [String[]](Get-ChildItem ([Environment]::GetFolderPath('MyDocuments')+'\psClick\psClick_UserData\ImageReader_Data')|Select BaseName -Unique).BaseName
         $collection.Add(([Management.Automation.ValidateSetAttribute]::new($validationSet)))  
 
-        $param = [Management.Automation.RuntimeDefinedParameter]::new('Base', [string], $collection)
+        $param = [Management.Automation.RuntimeDefinedParameter]::new('Name', [string], $collection)
         $dictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
-        $dictionary.Add('Base', $param)  
+        $dictionary.Add('Name', $param)  
         return $dictionary
     }
     end{
         try{
-            $Base = $PSBoundParameters.Base
-            $BaseSymbol = [psClick.Readtext]::LoadSymbolBase("$PSScriptRoot\ImageReader_Data\$Base.pssb")
-            $cfg = Get-Content "$PSScriptRoot\ImageReader_Data\$Base.cfg"|ConvertFrom-Json
+            $Base = $PSBoundParameters.Name
+            $BaseSymbol = [psClick.Readtext]::LoadSymbolBase(([Environment]::GetFolderPath('MyDocuments')+"\psClick\psClick_UserData\ImageReader_Data\$Base.pssb"))
+            $cfg = Get-Content ([Environment]::GetFolderPath('MyDocuments')+"\psClick\psClick_UserData\ImageReader_Data\$Base.cfg")|ConvertFrom-Json
         }
         catch{
             throw $_
         }
-        if($Path -is [System.Drawing.Image]){
-            $result = [psClick.Readtext]::Recognize($Path, $cfg.Settings, $cfg.txtColors, $cfg.bgColors, $BaseSymbol)
-        }
-        else{
-            $img = Get-Image -Path $path
-            $result = [psClick.Readtext]::Recognize($img, $cfg.Settings, $cfg.txtColors, $cfg.bgColors, $BaseSymbol)
-            $img.Dispose()
-        }
         [PSCustomObject]@{
-            Symbols     = $result
-            Text        = [psClick.Readtext]::SymbolsToString($result, $cfg.SpaceSize)
-            ImageOutput = [psClick.Readtext]::Output
+            Name   = $Base
+            Base   = $BaseSymbol
+            Config = $cfg
         }
+    }
+}
+
+function Recognize-Text 
+{
+    #.COMPONENT
+    #1.1
+    #.SYNOPSIS
+    #Author: Fors1k, Cirus ; Link: https://psClick.ru
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)][Alias("Image")]
+        $Path,
+        [Parameter(Mandatory,Position=1)]
+        $Base
+    )
+    if($Path -is [System.Drawing.Image]){
+        $result = [psClick.Readtext]::Recognize($Path, $Base.Config.Settings, $Base.Config.txtColors, $Base.Config.bgColors, $Base.Base)
+    }
+    else{
+        $img = Get-Image -Path $path
+        $result = [psClick.Readtext]::Recognize($img, $Base.Config.Settings, $Base.Config.txtColors, $Base.Config.bgColors, $Base.Base)
+        $img.Dispose()
+    }
+    [PSCustomObject]@{
+        Symbols     = $result
+        Text        = [psClick.Readtext]::SymbolsToString($result, $Base.Config.SpaceSize)
+        ImageOutput = [psClick.Readtext]::Output
     }
 }

6226cf48d6afac1e786bfd856a6f671d9c83e41d
Fors1k <68744677+Fors1kGato@users.noreply.github.com>
2022-06-07 03:06:21 +0300

    Delete Start-Psclick.ps1

--- psClick_Main/Start-Psclick.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Start-Psclick{ 
-    #.COMPONENT
-    #1
-    #.SYNOPSIS
-    #Author: Fors1k ; Link: https://psClick.ru
-    Param(
-    )
-    start "$psscriptroot\psCLick.exe" -WorkingDirectory $psscriptroot
-}
